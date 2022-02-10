ff711aea0603be90f54318cdd9f87b5f827d5489
Savannah <savmaxwell@gmail.com>
2022-02-10 11:10:48 -0700

    wip

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ if(process.argv[2] === 'web49') {
 }
 
 console.log(`the user is ${user}`)
+
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+    res.json({ message: 'hey there'})
+})
+
+const PORT = 9000;
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
